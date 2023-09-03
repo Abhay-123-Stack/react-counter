e7c98b71ccb8402a6527cb29a7d6702d65d9f616
Sayantan <sp@unthread.world>
2023-09-03 14:06:35 +0530

    useEffect Upldate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
 
-  console.log("A", name);
+  console.log("A");
 
+  // Component On Mount
+  // Component On Update
   useEffect(() => {
-    // console.log("C", count);
-  });
+    console.log("C", count, name);
+  }, [count, name]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <input type="text" value={name} onChange={(e) => {
-          console.log(e.target.value);
-          setName(e.target.value)}} />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setName(e.target.value);
+            console.log("D");
+          }}
+        />
 
         <p>{count}</p>
         <button
           onClick={() => {
             setCount(count + 1);
-            // console.log("B", count);
-          }}
-        >
+            console.log("B");
+          }}>
           Increment
         </button>
       </header>
